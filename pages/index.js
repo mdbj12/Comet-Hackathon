e3be05e6b74d1ed48a937ffabc3007b8ba3be07b
Michael Jeung <84149349+mdbj12@users.noreply.github.com>
2023-02-10 17:04:28 -0500

    change

--- pages/index.js
+++ pages/index.js
@@ -65,7 +65,7 @@ export default function Home() {
               <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto max-w-2xl">
                   <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                    I am so confused????
+                    I am so confused??????
                   </h1>
 
                   <div className="marquee">
