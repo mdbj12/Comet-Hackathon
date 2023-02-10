95898fabda4557e09337e56c576fa4b8d3c89ffc
Michael Jeung <84149349+mdbj12@users.noreply.github.com>
2023-02-10 16:00:11 -0500

    confusion

--- pages/index.js
+++ pages/index.js
@@ -91,7 +91,7 @@ export default function Home() {
                       <div className="bg-white rounded-md shadow-2xl ring-1 ring-gray-900/10">
                         <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-6 sm:p-6 rounded-md">
                           <h2 className="text-xl font-bold tracking-tight text-gray-800 sm:text-2xl">
-                            Here's a token of my love and affection,
+                            Here's a token of my confusion,
                           </h2>
                           {/* <div className="-m-3 flex items-start rounded-lg p-3 transition duration-150 ease-in-out hover:bg-gray-50">
                             <div className="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-md bg-slate-600 text-white sm:h-12 sm:w-12">
